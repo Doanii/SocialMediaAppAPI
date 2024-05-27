f2e10aaeb00aeab4a4aa31350886e78f019fb567
Daan Zieverink <daanzieverink@icloud.com>
2024-05-27 13:52:17 +0200

    Add ActivityService class for creating user activities.
    A new ActivityService class was added to handle the creation of user activities in the application. It includes a method to create an activity with specified content and type, which is then saved to the database.

---
+++ SocialMediaAppAPI/Services/ActivityService.cs
@@ -0,0 +1,35 @@
+﻿using SocialMediaAppAPI.Data;
+using SocialMediaAppAPI.Models;
+using SocialMediaAppAPI.Types.Enum;
+using System;
+using System.Threading.Tasks;
+
+namespace SocialMediaAppAPI.Services
+{
+    public class ActivityService
+    {
+        private readonly APIDbContext _dbContext;
+
+        public ActivityService(APIDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<string> CreateActivity(User user, ActivityEnum type, string content)
+        {
+            Activity activity = new Activity
+            {
+                Id = Guid.NewGuid(),
+                Content = content,
+                Type = type,
+                User = user,
+                UserId = user.Id
+            };
+
+            _dbContext.Activities.Add(activity);
+            await _dbContext.SaveChangesAsync();
+
+            return "Activity Generated!";
+        }
+    }
+}
