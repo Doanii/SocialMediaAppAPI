06307cf3e2ff8a5a65275c945a9ff40e6b60db2f
PepijnDiks <0336884@student.rocvantwente.nl>
2024-06-04 12:11:50 +0200

    IsLiked boolean and no longer needing to provide userid when liking, fixed query for likes where it didn't check per user

--- SocialMediaAppAPI/Controllers/LikesController.cs
+++ SocialMediaAppAPI/Controllers/LikesController.cs
@@ -8,11 +8,13 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration.UserSecrets;
 using SocialMediaAppAPI.Data;
 using SocialMediaAppAPI.Models;
+using SocialMediaAppAPI.Types.Attributes;
 
 namespace SocialMediaAppAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [ValidateApiToken]
     public class LikesController : ControllerBase
     {
         private readonly APIDbContext _context;
@@ -20,6 +22,11 @@ namespace SocialMediaAppAPI.Controllers
         public LikesController(APIDbContext context)
         {
             _context = context;
+        }
+
+        private User GetAuthenticatedUser()
+        {
+            return HttpContext.Items["AuthenticatedUser"] as User;
         }
 
         // GET: api/Likes/00000-00000-00000-00000
@@ -31,16 +38,24 @@ namespace SocialMediaAppAPI.Controllers
 
         // POST: api/Likes/00000-00000-00000-00000
         [HttpPost("{postId}")]
-        public async Task<ActionResult<bool>> LikePost(Guid postId, Guid userId)
+        public async Task<ActionResult<bool>> LikePost(Guid postId)
         {
-            Likes like = await _context.Likes.Where(l => l.PostId == postId).FirstAsync();
+            var authenticatedUser = GetAuthenticatedUser();
+            if (authenticatedUser == null)
+            {
+                return Unauthorized();
+            }
+
+            Likes like = await _context.Likes
+                .Where(l => l.PostId == postId && l.UserId == authenticatedUser.Id)
+                .FirstOrDefaultAsync();
 
             if (like == null)
             {
                 Likes newLike = new()
                 {
                     PostId = postId,
-                    UserId = userId,
+                    UserId = authenticatedUser.Id,
                     LikedAt = DateTime.Now,
                 };
 
--- SocialMediaAppAPI/Types/Requests/PostDTO.cs
+++ SocialMediaAppAPI/Types/Requests/PostDTO.cs
@@ -8,6 +8,7 @@ namespace SocialMediaAppAPI.Types.Requests
         public string Content { get; set; }
         public string OPUsername { get; set; }
         public bool Following { get; set; }
+        public bool IsLiked { get; set; }
         public int LikeCount { get; set; }
         public int CommentCount { get; set; }
         public DateTime CreatedAt { get; set; }
