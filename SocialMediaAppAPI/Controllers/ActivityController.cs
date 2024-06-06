6c47a5452d9e6d526d0accf7699d64d25d9f6d6c
Daan Zieverink <daanzieverink@icloud.com>
2024-06-06 23:50:54 +0200

    Add ActivityController and ActivityDTO classes for API functionality
    - Added ActivityController class with GetActivities method to retrieve user-specific activities.
    - Created ActivityDTO class to define activity data transfer object structure.

---
+++ SocialMediaAppAPI/Controllers/ActivityController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialMediaAppAPI.Data;
+using SocialMediaAppAPI.Models;
+using SocialMediaAppAPI.Types.Attributes;
+using SocialMediaAppAPI.Types.Requests;
+
+namespace SocialMediaAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ValidateApiToken]
+    public class ActivityController(APIDbContext _dbContext) : ControllerBase
+    {
+        private User GetAuthenticatedUser()
+        {
+            return HttpContext.Items["AuthenticatedUser"] as User;
+        }
+
+        [HttpGet]
+        public async Task<List<ActivityDTO>> GetActivities()
+        {
+            User user = GetAuthenticatedUser();
+            return await _dbContext.Activities.Select(a => new ActivityDTO
+            {
+                UserId = a.UserId,
+                CreatedAt = a.CreatedAt,
+                Content = a.Content,
+                Type = a.Type,
+            }).Where(a => a.UserId == user.Id).OrderByDescending(u => u.CreatedAt).ToListAsync();
+        }
+    }
+}
---
+++ SocialMediaAppAPI/Types/Requests/ActivityDTO.cs
@@ -0,0 +1,12 @@
+﻿using SocialMediaAppAPI.Types.Enum;
+
+namespace SocialMediaAppAPI.Types.Requests
+{
+    public class ActivityDTO
+    {
+        public string Content {  get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public ActivityEnum Type { get; set; }
+    }
+}
